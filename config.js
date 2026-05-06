ec70d36de446d8277c78974d6f78f540a3830d5f
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2026-05-06 17:55:38 -0500

    Arreglo 2

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBt8Hd-RjdHByRVoDte4RUKGnPpwBoERy4",
+  authDomain: "life-quest-413ab.firebaseapp.com",
+  projectId: "life-quest-413ab",
+  storageBucket: "life-quest-413ab.firebasestorage.app",
+  messagingSenderId: "316851758197",
+  appId: "1:316851758197:web:5663d50879dcb0698d843c"
+}
